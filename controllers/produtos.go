55fb5c542fa908473f447f6b28559709c8832438
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-08-20 15:17:35 -0300

    Insert produtos into DB

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"Loja/models"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -12,4 +14,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preco", err)
+		}
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão do preco", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
 }
